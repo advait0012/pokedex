05d68e8c298bea2a5320a2f92f9265ef6da23661
advait0012 <advait0012@proton.me>
2024-10-02 08:10:26 +0000

    cards done

---
+++ js/main.js
@@ -0,0 +1,24 @@
+import data from "./data.json"
+
+const dataRow = document.querySelector("[data-pokemon-row]")
+
+data.forEach(Pokemon)
+
+function Pokemon(i){
+    const div = document.createElement("div");
+    div.classList.add("col")
+    div.innerHTML = `<div class="card">
+            <img
+              src="${i.image}"
+              class="card-img-top"
+              alt="${i.name}"
+            />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">"${i.description}"</p>
+              <a href="${i.link}" class="btn btn-primary">Visit</a>
+            </div>
+          </div>`
+          dataRow.appendChild(div)
+}
+
